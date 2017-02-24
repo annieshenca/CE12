3939a987f21c47224a9e953ba9bbec266d0cfb42
Annie Shen <annieshenca@eduroam-169-233-239-58.ucsc.edu>
2017-02-23 18:48:46 -0800

    lab4

--- ce12/lab4/Lab4.asm
+++ ce12/lab4/Lab4.asm
@@ -3,8 +3,11 @@
 TOP
 	AND R0,R0,0			;clear all registers
 	AND R1,R1,0
+
 	AND R2,R2,0
+
 	AND R3,R3,0
+
 	AND R4,R4,0
 	AND R5,R5,0
 	ST R1, FLAG
@@ -16,40 +19,42 @@ INPUT
 	GETC				;get the inputted character and store into R0
 	OUT
 	LD R1,LF			;load value of LF into R1
-	ADD R1,R0,R1
-	BRz FLAG_CALC		;if R0 is LF, then branch to calculate FLAG's value
-	
-	LD R1,X 			;R1 = -88
-	ADD R1,R0,R1		;R1 = R0-88
-	BRz FLAG_X			;if input is "X", then branch to FLAG_X
-	LD R1,E 			;load value of E(-69) into R1
-	ADD R1,R0,R1		;R1 = R0-69
-	BRz FLAG_E			;FLAG = 1
-	LD R1,D
-	ADD R1,R0,R1
-	BRz FLAG_D
 
+	ADD R1,R0,R1
+
+	BRz LF				;if R0 is LF, then branch to calculate FLAG's value
+	
+	LD R1,E 	 		;load value of E(-69) into R1
+	ADD R1,R0,R1		;R1 = R0-69
+
+	BRz FLAG_E			;branch to FLAG_E to change FLAG to 1
+	LD R1,D 			;load value of D(-68) into R1
+	ADD R1,R0,R1 		;R1 = R0-68
+	BRz FLAG_D 			;branch to FLAG_D to change FLAG to -1
+	
+	LD R1,X				;load value of X(-88) into R1
+	ADD R1,R0,R1		;R1 = R0-88
+	BRz INPUT			;if input is "X", let FLAG remain 0, then branch to INPUT
 	BRnp TOP			;branch to TOP to restart if the user entered in something not expected
 
-FLAG_X
-	ST 0,FLAG 			;store 0 into FLAG
-	BRnzp INPUT 		;branch back to INPUT to sense LF
-
 FLAG_E
-	ST -1,FLAG 			;store -1 into FLAG
-	BRnzp INPUT
+	LD R1,FLAG			;laod value of FLAG(0) into R1
+	ADD R1,R1,1 		;R1 = R1+1 = 1
+	ST R1,FLAG			;store value of R1 into FLAG
+	BRnzp INPUT			;branch back to INPUT
 
 FLAG_D
-	ST 1,FLAG 			;store 1 into FLAG
-	BRnzp INPUT
+	LD R1,FLAG		;laod value of FLAG(0) into R1
+	ADD R1,R1,-1 		;R1 = R1-1 = -1
+	ST R1,FLAG			;store value of R1 into FLAG
+	BRnzp INPUT			;branch back to INPUT
 
-FLAG_CALC
+LF
 	LD R1, FLAG 		;load value of FLAG into R1
-	BRz EXIT			;if FLAG==0, meaning user entered "X" to exit 
-
+	BRz EXIT			;if FLAG==0, meaning user entered "X" to exit
 	LEA R1,PROMPT		;load PROMPT into R1
 	PUTS				;print out asking for cipher
-	BRnzp DONE 	;TESTING
+	BRnzp DONE			;TESTING
 
 DONE
 	LEA R1,EMPT
@@ -60,18 +65,19 @@ EXIT
 	LEA R1,BYE
 	PUTS
 
+
 HALT
 
-WEL		.STRINGZ "Welcome to Annie's Caesar Cipher program"
+;Variables
+WEL	.STRINGZ "Welcome to Annie's Caesar Cipher program\n"
 PROMPT	.STRINGZ "Do you want to (E)ncrypt, (D)ecrypt, or (E)xit?\n"
-LF		.FILL #-10
-E 		.FILL #-69
-D 		.FILL #-68
-X 		.FILL #-88
+E 	.FILL #-69
+D 	.FILL #-68
+X 	.FILL #-88
 FLAG	.FILL #0
 ASKCI	.STRINGZ "What is the cipher(1-25)?\n"
 CIPHER	.FILL #0
-ASKSTR	.FILL "What is the string(200 characters max)?\n"
+ASK	.STRINGZ "What is the string(200 characters max)?\n"
 NEGA	.FILL #-65
 NEGZ	.FILL #-90
 NEGa 	.FILL #-97
